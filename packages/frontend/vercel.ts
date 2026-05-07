d99599cf9b601bc60d52d0deba5beaf8ef945cb5
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2026-05-08 00:21:50 +0100

    fix: comment out unused routes and deployment environment variables in Vercel configuration

--- packages/frontend/vercel.ts
+++ packages/frontend/vercel.ts
@@ -1,18 +1,18 @@
 import {
   routes,
-  deploymentEnv,
+  // deploymentEnv,
   type VercelConfig,
-  Rewrite,
+  // Rewrite,
 } from "@vercel/config/v1";
 
 export const config: VercelConfig = {
   rewrites: [
-    routes.rewrite("/api/(.*)", `${deploymentEnv("BACKEND_URL")}/api/$1`, {
-      requestHeaders: {
-        authorization: `Bearer ${deploymentEnv("API_TOKEN")}`,
-      },
-    }) as Rewrite,
-    routes.rewrite("/auth/(.*)", `${deploymentEnv("BACKEND_URL")}/auth/$1`),
-    // routes.rewrite("/(.*)", "/"),
+    // routes.rewrite("/api/(.*)", `${deploymentEnv("BACKEND_URL")}/api/$1`, {
+    //   requestHeaders: {
+    //     authorization: `Bearer ${deploymentEnv("API_TOKEN")}`,
+    //   },
+    // }) as Rewrite,
+    // routes.rewrite("/auth/(.*)", `${deploymentEnv("BACKEND_URL")}/auth/$1`),
+    routes.rewrite("/(.*)", "/"),
   ],
 };
