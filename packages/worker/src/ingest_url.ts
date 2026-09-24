8d0ebab35a61bdc2f3caed38c08453e0732fb7f1
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2026-09-24 23:25:45 +0100

    fix: wrap response fetching in try-catch for better error handling

--- packages/worker/src/ingest_url.ts
+++ packages/worker/src/ingest_url.ts
@@ -5,26 +5,31 @@ import { prisma } from "@weather-data-app/database";
 import { ingestStationFile } from "./index.js";
 
 async function ingestUrl() {
-  const response = await new Promise<IncomingMessage>((resolve, reject) => {
-    get(
-      new URL(
-        "https://www.ncei.noaa.gov/data/daily-summaries/archive/daily-summaries-latest.tar.gz",
-      ),
-      {
-        method: "GET",
-      },
-      (res) => {
-        res.on("error", reject);
-        resolve(res);
-      },
-    );
-  });
+  try {
+    const response = await new Promise<IncomingMessage>((resolve, reject) => {
+      get(
+        new URL(
+          "https://www.ncei.noaa.gov/data/daily-summaries/archive/daily-summaries-latest.tar.gz",
+        ),
+        {
+          method: "GET",
+        },
+        (res) => {
+          res.on("error", reject);
+          resolve(res);
+        },
+      );
+    });
 
-  ingestStationFile({
-    source: response,
-    startDate: new Date("2026-05-03"),
-    endDate: new Date(""),
-  });
+    ingestStationFile({
+      source: response,
+      startDate: new Date("2026-05-03"),
+      endDate: new Date(""),
+    });
+  } catch (error) {
+    console.error("Injest URL Error");
+    console.error(error);
+  }
 }
 
 ingestUrl()
